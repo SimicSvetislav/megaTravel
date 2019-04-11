dc81eb7c2da79fd26658229b69c0b8425ea2d574
SimicSvetislav <simicsvetislav@gmail.com>
2019-04-12 00:42:39 +0200

    Initial main backend application

---
+++ backend-main/src/main/java/com/project/megaTravel/MegaTravelApplication.java
@@ -0,0 +1,13 @@
+package com.project.megaTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaTravelApplication.class, args);
+	}
+
+}
