d4abd4de8aab8c6e133fd26247de281014e2cceb
SimicSvetislav <simicsvetislav@gmail.com>
2019-04-14 13:39:31 +0200

    Konfigurisanje

---
+++ backend-main/src/main/java/com/project/megatravel/app/MegatravelApplication.java
@@ -0,0 +1,14 @@
+package com.project.megatravel.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegatravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegatravelApplication.class, args);
+		System.out.println("\nMain backend application successfully started.");
+	}
+
+}
--- backend-main/src/test/java/com/project/megaTravel/MegaTravelApplicationTests.java
+++ backend-main/src/test/java/com/project/megaTravel/MegaTravelApplicationTests.java
@@ -1,4 +1,4 @@
-package com.project.megaTravel;
+package com.project.megatravel;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MegaTravelApplicationTests {
+public class MegatravelApplicationTests {
 
 	@Test
 	public void contextLoads() {
