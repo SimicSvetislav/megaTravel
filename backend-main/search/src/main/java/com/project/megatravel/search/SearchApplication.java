569cc4019befd636b223d3955cbabff68684ec77
SimicSvetislav <simicsvetislav@uns.ac.rs>
2019-06-21 20:26:11 +0200

    Podešavanje komunikacije sa bazom podataka u okviru servisa za pretragu

--- backend-main/search/src/main/java/com/project/megatravel/search/SearchApplication.java
+++ backend-main/search/src/main/java/com/project/megatravel/search/SearchApplication.java
@@ -1,5 +1,7 @@
 package com.project.megatravel.search;
 
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,9 +9,23 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SearchApplication {
-
+	
+	private final static Logger logger = Logger.getLogger(SearchApplication.class.getName());
+	
 	public static void main(String[] args) {
+		
+		logger.info("Connecting to database");
+		
+		try {
+			ExistDB.initDatabase();
+			logger.info("Connection with database established");
+		} catch (Exception e) {
+			logger.warning("Can't connect to database");
+		}
+		
 		SpringApplication.run(SearchApplication.class, args);
+		
+		logger.info("Search microservice successfully started");
 	}
 
 }
